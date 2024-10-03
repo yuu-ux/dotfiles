21014d2c0d893aa17555a2c45eac52eeb639ea25
yuu-ux <yuugaehara@gmail.com>
2024-10-04 08:41:08 +0900

    feat: .zshrcとnvimのinit.luaとraycastを追加 ==== Prefix ==== - feat: 新しい機能 - fix: バグの修正 - docs: ドキュメントのみの変更 - style: 空白、フォーマット、セミコロン追加など - refactor: 仕様に影響がないコード改善(リファクタ) - perf: パフォーマンス向上関連 - test: テスト関連 - chore: ビルド、補助ツール、ライブラリ関連 - wip: 一時中断

---
+++ .zshrc
@@ -0,0 +1,172 @@
+alias add='git add'
+alias commit='git commit'
+alias push='git push'
+alias v='nvim'
+alias examshell='bash -c "$(curl https://grademe.fr)"'
+alias cdalgo='/Users/ebarayuuga/Documents/algosiki'
+alias nor='/Users/ebarayuuga/Library/Python/3.11/bin/norminette'
+alias cdso='cd /Users/ebarayuuga/Documents/so_long'
+# Created by `pipx` on 2024-08-09 13:57:16
+export PATH="$PATH:/Users/ebarayuuga/.local/bin"
+alias j=z
+. ~/z/z.sh
+. /usr/local/opt/asdf/libexec/asdf.sh
+source ~/.zsh/git-prompt.sh
+
+GIT_PS1_SHOWDIRTYSTATE=true
+GIT_PS1_SHOWUNTRACKEDFILES=true
+GIT_PS1_SHOWSTASHSTATE=true
+GIT_PS1_SHOWUPSTREAM=auto
+
+setopt PROMPT_SUBST
+PROMPT='%F{yellow}%D %*%f%F{blue}(%M)%f: %F{cyan}%~%f %F{red}$(__git_ps1 "(%s)")%f
+%F{green}%n%f %% '
+## lang
+export LANG=ja_JP.UTF-8
+export LC_ALL=ja_JP.UTF-8
+
+# color
+autoload -Uz colors
+colors
+local WHITE=$'%{\e[97m%}'
+local RED=$'%{\e[91m%}'
+local GREEN=$'%{\e[92m%}'
+local YELLOW=$'%{\e[93m%}'
+local BLUE=$'%{\e[94m%}'
+local CYAN=$'%{\e[96m%}'
+local CLEAR=$'%{\e[0m%}'
+
+setopt auto_menu            # 補完キー（Tab, Ctrl+I) を連打するだけで順に補完候補を自動で補完する
+setopt auto_param_slash     # ディレクトリ名の補完で末尾の / を自動的に付加し、次の補完に備える
+setopt auto_param_keys      # カッコの対応などを自動的に補完する
+setopt auto_list            # 補完候補が複数ある時に、一覧表示する
+setopt list_packed          # 補完候補をできるだけ詰めて表示する
+setopt list_types           # auto_list の補完候補一覧で、ls -F のようにファイルの種別をマーク表示
+setopt mark_dirs            # ファイル名の展開でディレクトリにマッチした場合末尾に / を付加する
+setopt interactive_comments # コマンドラインでも # 以降をコメントと見なす
+setopt magic_equal_subst    # コマンドラインの引数で --prefix=/usr などの = 以降でも補完できる
+setopt always_last_prompt   # カーソル位置を保持したままファイル名一覧を順次補完する
+setopt extended_glob        # 拡張グロブで補完する（ファイル名で #, ~, ^ の 3 文字を正規表現として扱う）
+setopt globdots             # ドットの指定なしでも.から始まるファイルを補完する
+
+# setting
+setopt auto_cd              # 指定したコマンド名がなく、ディレクトリ名と一致した場合 cd する
+setopt auto_pushd           # 移動したディレクトリを記録し、cd -[TAB]で候補を提示する
+setopt pushd_ignore_dups    # auto_pushd で重複ディレクトリを記録しない
+setopt hist_ignore_all_dups # 重複履歴を残さない
+setopt auto_resume          # サスペンド中のプロセスと同じコマンド名を実行した場合はリジュームする
+setopt nobeep nolistbeep    # ビープ音を鳴らさないようにする
+setopt correct              # コマンドのスペルチェックをする
+setopt ignore_eof           # Ctrl+D では終了しないようになる（exit, logout などを使う）
+setopt long_list_jobs       # 内部コマンド jobs の出力をデフォルトで jobs -l にする
+setopt multios              # 複数のリダイレクトやパイプなど、必要に応じて tee や cat の機能が使われる
+setopt numeric_glob_sort    # ファイル名の展開で、辞書順ではなく数値的にソートされるようになる
+setopt print_eightbit       # 8 ビット目を通すようになり、日本語のファイル名などを見れるようになる
+setopt print_exit_value     # 戻り値が 0 以外の場合終了コードを表示する
+setopt short_loops          # for, repeat, select, if, function などで簡略文法が使えるようになる
+setopt prompt_subst         # 色を使う
+unsetopt promptcr           # 文字列末尾に改行コードが無い場合でも表示する
+setopt transient_rprompt    # コピペする際に右プロンプトを非表示にする
+setopt notify
+
+# history
+HISTFILE=~/.zsh_history   # ヒストリを保存するファイル
+HISTSIZE=10000            # メモリー上のヒストリ数
+SAVEHIST=100000           # ファイル上のヒストリ数
+setopt extended_history   # zsh の開始・終了時刻をヒストリファイルに書き込む
+setopt share_history      # 他のシェルのヒストリをリアルタイムで共有する
+setopt hist_reduce_blanks # 余分なスペースを削除してヒストリに保存する
+setopt hist_ignore_space  # コマンドラインの先頭がスペースで始まる場合ヒストリに追加しない
+setopt hist_verify        # ヒストリを呼び出してから実行する間に一旦編集できる状態になる
+setopt hist_no_store      # history (fc -l) コマンドをヒストリリストから取り除く。
+setopt append_history     # 複数の zsh を同時に使う時など history ファイルに上書きせず追加する
+autoload history-search-end
+
+if [[ ${SET_LOCAL_LD_LIBRARY_PATH} -ne 1 && -d $HOME/local/bin ]]; then
+  export SET_LOCAL_LD_LIBRARY_PATH=1
+  export LD_LIBRARY_PATH=$HOME/local/lib:$LD_LIBRARY_PATH
+fi
+
+# completion
+autoload -Uz compinit
+compinit -u
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' # 大文字と小文字の区別しない
+zstyle ':completion:*:default' menu select=2
+
+# Ctrl+rでヒストリーのインクリメンタルサーチ、Ctrl+sで逆順
+bindkey '^r' history-incremental-pattern-search-backward
+bindkey '^s' history-incremental-pattern-search-forward
+
+# コマンド途中からヒストリー検索
+autoload -Uz history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "^p" history-beginning-search-backward-end
+bindkey "^b" history-beginning-search-forward-end
+
+autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+zstyle ":chpwd:*" recent-dirs-default true
+zstyle ':completion:*:*:cdr:*:*' menu selection
+bindkey -e
+
+# export EDITOR
+if [ -f $HOME/.asdf/shims/nvim ]; then
+  export EDITOR=$HOME/.asdf/shims/nvim;
+elif [ -f $HOME/local/bin/vim ]; then
+  export EDITOR=$HOME/local/bin/vim
+elif [ -f /usr/local/bin/vim ]; then
+  export EDITOR=/usr/local/bin/vim
+elif [ -f /usr/bin/vim ]; then
+  export EDITOR=/usr/bin/vim
+fi
+
+# alias
+alias ls='ls -F --color=auto'
+alias la='ls -Fa --color=auto'
+alias ll='ls -FalG --color=auto'
+alias mysql='mysql --auto-rehash'
+alias vi=$EDITOR
+alias v='nvim'
+
+# global alias
+alias -g L='| less'
+alias -g G='| grep'
+alias -g F='| fzf'
+alias -g W='| wc -l'
+alias -g T='| tail'
+alias -g H='| head'
+
+# 追加したパッケージのコマンドを優先
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_CACHE_HOME=$HOME/.cache
+export XDG_DATA_HOME=$HOME/.local/share
+
+# fzf
+if version_fzf="$(asdf current fzf 2>/dev/null | perl -pe 's/[^\s]+\s+([^\s]+)\s+[^\s]+/$1/')" && [ "${version_fzf}x" != "x" ]; then
+  if [[ ! "$PATH" == *${HOME}/.asdf/installs/fzf/${version_fzf}/bin* ]]; then
+    export PATH="${PATH:+${PATH}:}${HOME}/.asdf/installs/fzf/${version_fzf}/bin"
+  fi
+  [[ $- == *i* ]] && source "${HOME}/.asdf/installs/fzf/${version_fzf}/shell/completion.zsh" 2>/dev/null
+  [ -f "${HOME}/.asdf/installs/fzf/${version_fzf}/shell/key-bindings.zsh" ] && source "${HOME}/.asdf/installs/fzf/${version_fzf}/shell/key-bindings.zsh" 2>/dev/null
+  [ -f "${HOME}/settings/dotfiles/.zsh/fzf_function.zsh" ] && source "${HOME}/settings/dotfiles/.zsh/fzf_function.zsh" 2>/dev/null
+  export FZF_DEFAULT_OPTS='--height 40% --reverse'
+  export FZF_TMUX_OPTS='-p'
+fi
+
+tm() {
+  [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
+  if [ $1 ]; then
+    tmux $change -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -t "$1"); return
+  fi
+  session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -t "$session" || echo "No sessions found."
+}
+
+[[ -e /usr/share/terminfo/x/xterm-256color ]] && export TERM=screen-256color
+[[ -e /lib/terminfo/x/xterm-256color ]] && export TERM=screen-256color
+
+
+PATH="/Users/ebarayuuga/perl5/bin${PATH:+:${PATH}}"; export PATH;
+PERL5LIB="/Users/ebarayuuga/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
+PERL_LOCAL_LIB_ROOT="/Users/ebarayuuga/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
+PERL_MB_OPT="--install_base \"/Users/ebarayuuga/perl5\""; export PERL_MB_OPT;
+PERL_MM_OPT="INSTALL_BASE=/Users/ebarayuuga/perl5"; export PERL_MM_OPT;
---
+++ nvim/init.lua
@@ -0,0 +1,201 @@
+-- 自動で読み込まれるものをオフにする・文字コード指定
+vim.cmd('autocmd!')
+vim.scriptencoding   = "utf-8"
+
+-- 無効にするビルトインプラグイン
+vim.g["loaded_2html_plugin"] = 1
+vim.g["loaded_compiler"] = 1
+vim.g["loaded_bugreport"] = 1
+vim.g["loaded_ftplugin"] = 1
+vim.g["loaded_getscript"] = 1
+vim.g["loaded_getscriptPlugin"] = 1
+vim.g["loaded_gzip"] = 1
+vim.g["loaded_logipat"] = 1
+vim.g["loaded_matchit"] = 1
+vim.g["loaded_netrw"] = 1
+vim.g["loaded_netrwPlugin"] = 1
+vim.g["loaded_netrwSettings"] = 1
+vim.g["loaded_netrwFileHandlers"] = 1
+vim.g["loaded_optwin"] = 1
+vim.g["loaded_rplugin"] = 1
+vim.g["loaded_rrhelper"] = 1
+vim.g["loaded_spellfile_plugin"] = 1
+vim.g["loaded_synmenu"] = 1
+vim.g["loaded_tar"] = 1
+vim.g["loaded_tarPlugin"] = 1
+vim.g["loaded_tutor"] = 1
+vim.g["loaded_vimball"] = 1
+vim.g["loaded_vimballPlugin"] = 1
+vim.g["loaded_zip"] = 1
+vim.g["loaded_zipPlugin"] = 1
+
+
+-- Plugin management using vim-plug
+vim.cmd([[
+    call plug#begin('~/.config/nvim/plugged')
+        Plug 'nvim-lua/plenary.nvim'
+        Plug 'nvim-telescope/telescope.nvim', { 'tag': '0.1.8' }
+        Plug 'nvim-telescope/telescope-fzf-native.nvim', { 'do': 'make' }
+        Plug 'nvim-lualine/lualine.nvim'
+        Plug 'nvim-tree/nvim-web-devicons'
+        Plug 'joshdick/onedark.vim'
+        Plug 'pbondoer/vim-42header'
+    call plug#end()
+]])
+
+-- Enable syntax highlighting
+vim.cmd("syntax on")
+
+-- Set colorscheme
+vim.cmd("colorscheme onedark")
+
+-- settings
+vim.opt.mouse       = ""                          -- マウスを無効にする
+vim.opt.clipboard   = "unnamedplus"               -- システムクリップボードを使用する
+vim.opt.swapfile    = false                       -- スワップファイルを作らない
+vim.opt.backup      = false                       -- バックアップファイルを作らない
+vim.opt.writebackup = false                       -- ファイル上書き前にバックアップを作成しない
+vim.opt.undofile    = false                       -- アンドゥファイルを作成しない
+vim.opt.completeopt = 'menuone,noinsert,noselect' -- 補完メニューを設定する
+
+vim.opt.encoding      = "utf-8"                          -- neovim 内の文字エンコーディングを指定する
+vim.opt.fileencoding  = "utf-8"                          -- デフォルトの文字コードを指定する
+vim.opt.fileencodings = "utf-8,euc-jp,sjis,iso-2022-jp"  -- 文字コードの自動認識順序を指定する
+vim.opt.fileformat    = "unix"                           -- デフォルトの改行コードを指定する
+vim.opt.fileformats   = "unix,dos,mac"                   -- 改行コードの自動認識順序を指定する
+
+vim.opt.showmode       = false                                 -- モードを非表示にする
+vim.opt.showtabline    = 0                                     -- タブラインの表示を指定する（「0」だと表示されない）
+vim.opt.number         = true                                  -- 行番号を表示する
+vim.opt.relativenumber = false                                 -- 相対行番号表示を表示しない
+vim.opt.numberwidth    = 4                                     -- 行番号の桁数を指定する
+vim.opt.showmatch      = true                                  -- 対応する括弧のハイライト表示する
+vim.opt.foldmethod     = "marker"                              -- 折り畳みのマーカーにデフォルトの「{{{」「}}}」を使用する
+vim.opt.splitright     = true                                  -- 画面を縦分割する際に右に開く
+vim.opt.splitbelow     = true                                  -- 画面を横分割する際に下に開く
+vim.opt.linebreak      = true                                  -- 単語の分け目で改行する
+vim.opt.termguicolors  = true                                  -- 24ビットRGBカラーを有効にする
+vim.opt.laststatus     = 2                                     -- 常にステータスラインを表示する
+vim.opt.cmdheight      = 2                                     -- コマンドラインの行数を指定する
+vim.opt.list           = true                                  -- ホワイトスペースなどを表示する
+vim.opt.listchars      = "tab:>.,trail:_,extends:>,precedes:<" -- 不可視文字の表示形式を指定する
+vim.opt.cursorline     = true                                  -- カーソル行をハイライトする
+vim.opt.conceallevel   = 0                                     -- conceal 属性のテキストをどう表示するか指定する（「0」の場合、通常通り表示する）
+vim.opt.signcolumn     = "yes"                                 -- 各種記号用カラムを常に表示（vim-gitgutterで未修正ファイルを変更した際のずれをなくすため）
+vim.opt.winblend       = 10                                    -- フローティングウィンドウの透明度を指定する（「0」の場合、非透明になる）
+vim.opt.pumblend       = 10                                    -- ポップアップメニューの透明度を指定する（「0」の場合、非透明になる）
+vim.opt.scrolloff      = 8                                     -- スクロール時にカーソル行上下に表示する行数を指定する
+vim.opt.sidescrolloff  = 8                                     -- 横スクロール時にカーソルの左右に表示する桁数を指定する
+vim.opt.backspace      = "indent,eol,start"                    -- バックスペースでインデントなどを消せるようにする
+vim.opt.whichwrap      = "b,s,h,l,<,>,[,]"                     -- 行頭や行末で左右に移動した際に行をまたいで移動ができるようにする
+
+vim.opt.ignorecase = true -- 検索時に大文字小文字を区別しない
+vim.opt.smartcase  = true -- 検索時に大文字が含まれる場合のみ大文字小文字を区別する
+vim.opt.hlsearch   = true -- 前回の検索パターンが存在する場合にマッチするテキストを強調表示する
+
+vim.opt.expandtab   = true -- タブ入力を空白に変換する
+vim.opt.shiftwidth  = 4    -- 自動インデント時に入力する空白の数する
+vim.opt.tabstop     = 4    -- タブが返還される空白数を指定する
+vim.opt.smartindent = true -- 改行時に入力された行の末尾に合わせて次の行のインデントを増減する
+
+vim.opt.hidden     = true -- 未保存のファイルがある場合でも別ファイルを開くことができるようにする
+vim.opt.history    = 100  -- コマンド履歴の保存数を指定する
+vim.opt.lazyredraw = false -- コマンド実行中は再描画しない
+vim.opt.synmaxcol  = 240  -- シンタックスハイライトする文字数を指定する
+vim.opt.updatetime = 100  -- ms to wait for trigger an e
+
+vim.opt.shortmess:append("c")
+
+-- 保存時に末尾のスペースを削除する
+vim.api.nvim_create_autocmd("BufWritePre", { pattern = "*", command = ":%s/\\s\\+$//e", })
+
+---- ファイルタイプ ----
+vim.api.nvim_command("autocmd BufNewFile,BufRead *.ddl,*.sql setfiletype sql")
+vim.api.nvim_command("autocmd BufNewFile,BufRead *.tt,*.inc setfiletype tt2html")
+
+-- リーダーキー
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- リセット
+vim.keymap.set("v", "nn", "<esc>")
+vim.keymap.set("i", "jj", "<esc>")
+vim.keymap.set("", ";", "<Nop>", { noremap = true, silent = true, desc = "Disable ; key" })
+vim.keymap.set("", "m", "<Nop>", { noremap = true, silent = true, desc = "Disable m key" })
+vim.keymap.set("", ",", "<Nop>", { noremap = true, silent = true, desc = "Disable , key" })
+vim.keymap.set("", "#", "<Nop>", { noremap = true, silent = true, desc = "Disable # key" })
+vim.keymap.set("", "<Leader>n", "<Nop>", { noremap = true, silent = true, desc = "Disable <Leader>n key" })
+vim.keymap.set("", "<Space>", "<Nop>", { noremap = true, silent = true, desc = "Disable <Space> key" })
+vim.keymap.set("n", "Q", "", { noremap = true, silent = true, desc = "Disable Q key" })
+vim.keymap.set("n", "<Esc><Esc>", ":nohl<CR>", { noremap = true, silent = true, desc = "Clear search highlighting" })
+vim.keymap.set('n', 'd', '"_d', { noremap = true, silent = true })
+vim.keymap.set('x', 'd', '"_d', { noremap = true, silent = true })
+
+-- インデント
+vim.keymap.set("n", ">", ">>", { noremap = true, silent = true, desc = "Indent line to the right" })
+vim.keymap.set("n", "<", "<<", { noremap = true, silent = true, desc = "Indent line to the left" })
+vim.keymap.set("x", ">", ">gv", { noremap = true, silent = true, desc = "Indent selected lines to the right and reselect" })
+vim.keymap.set("x", "<", "<gv", { noremap = true, silent = true, desc = "Indent selected lines to the left and reselect" })
+
+-- talescope設定
+local telescope = require('telescope')
+local actions = require('telescope.actions')
+-- Load Telescope FZF extension
+telescope.load_extension('fzf')
+
+telescope.setup{
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-n>'] = actions.move_selection_next,
+        ['<C-p>'] = actions.move_selection_previous,
+        ['<Esc>'] = actions.close,
+      },
+    },
+  },
+  pickers = {
+    find_files = {
+      theme = "dropdown",
+    },
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false にすると完全一致での検索
+      override_generic_sorter = true,  -- デフォルトのジェネリックソーターを上書き
+      override_file_sorter = true,     -- デフォルトのファイルソーターを上書き
+      case_mode = "smart_case",        -- または "ignore_case" または "respect_case"
+    },
+  },
+}
+
+-- Telescope key mappings
+vim.api.nvim_set_keymap('n', '<leader>b', ':Telescope buffers<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>f', ':Telescope find_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>g', ':Telescope live_grep<CR>', { noremap = true, silent = true })
+-- ビジュアルモードで "p" に "_dP" をマッピング
+vim.api.nvim_set_keymap('x', 'p', '"_dP', { noremap = true, silent = true })
+
+-- lualine設定
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '|', right = '|' },
+    section_separators = { left = ' ', right = ' ' },
+    globalstatus = true,
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = {
+        {'filename', path = 1, symbols = {
+          modified = ' ●',      -- text to show when the file is modified.
+          readonly = ' ',       -- text to show when the file is non-modifiable or readonly.
+          unnamed = '[no name]', -- text to show for unnamed buffers.
+        }}
+    },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
+  }
+}
