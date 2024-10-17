29a01d27e598dc8c8468d6e4659f1b976f1f0e35
Yuga Ehara <yehara@c1r7s4.42tokyo.jp>
2024-10-17 22:38:48 +0900

    :heavy_plus_sign: .gitconfigにコミットテンプレートの読み込み先を追記するように変更

--- setting.sh
+++ setting.sh
@@ -1,6 +1,8 @@
+# vimplug インストール
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+# シンボリックリンク
 CURRENT_DIR=$(cd $(dirname $0); pwd)
 ln -snf ${CURRENT_DIR}/.zshrc ${HOME}/.zshrc
 for dir in *
@@ -10,5 +12,11 @@ do
     fi
 done
 
+# コミットテンプレート追加
+echo "
+[commit]
+  template = ~/dotfiles/.gitmessages" >> ${HOME}/.gitconfig
+
+# nvim プラグインインストール
 mkdir ${CURRENT_DIR}/nvim/plugged
 nvim +":PlugInstall" ${HOME}/dotfiles/nvim/init.lua
