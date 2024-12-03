b793fb3764e3b3b3382feef24a6958c6df1ef88c
yuu-ux <yuugaehara@gmail.com>
2024-12-04 01:56:46 +0900

    :recycle: lualine 調整

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,28 +1,32 @@
--- lualine設定
-require("lualine").setup {
-    options = {
-        icons_enabled = true,
-        theme = "auto",
-        component_separators = {left = "|", right = "|"},
-        section_separators = {left = " ", right = " "},
-        globalstatus = true
-    },
-    sections = {
-        lualine_a = {"mode"},
-        lualine_b = {"branch", "diff", "diagnostics"},
-        lualine_c = {
-            {
-                "filename",
-                path = 1,
-                symbols = {
-                    modified = "●",
-                    readonly = " ",
-                    unnamed = "[no name]"
-                }
-            }
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    lazy = false,
+    opts = {
+        options = {
+            icons_enabled = true,
+            theme = 'auto',
+            component_separators = {left = '|', right = '|'},
+            section_separators = {left = ' ', right = ' '},
+            globalstatus = true
         },
-        lualine_x = {"encoding", "fileformat", "filetype"},
-        lualine_y = {"progress"},
-        lualine_z = {"location"}
+        sections = {
+            lualine_a = {'mode'},
+            lualine_b = {'branch', 'diff', 'diagnostics'},
+            lualine_c = {
+                {
+                    'filename',
+                    path = 1,
+                    symbols = {
+                        modified = '●',
+                        readonly = ' ',
+                        unnamed = '[no name]'
+                    },
+                },
+            },
+            lualine_x = {'encoding', 'fileformat', 'filetype'},
+            lualine_y = {'progress'},
+            lualine_z = {'location'},
+        },
     }
 }
