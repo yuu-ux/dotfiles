c4e7b19bb7aea38b98bf5463530ada46b39ac924
yuu-ux <yuugaehara@gmail.com>
2024-12-19 21:49:37 +0900

    :heavy_plus_sign: hlchunk の設定を追加

---
+++ nvim/lua/plugins/hlchunk.lua
@@ -0,0 +1,19 @@
+return {
+    'shellRaining/hlchunk.nvim',
+    opts = {
+        chunk = {
+            enable = true,
+            style = '#806d9c',
+            chars = {
+                horizontal_line = '─',
+                vertical_line = '│',
+                left_top = '╭',
+                left_bottom = '╰',
+                right_arrow = '>'
+            },
+        },
+        indent = {
+            enable = true,
+        },
+    },
+}
