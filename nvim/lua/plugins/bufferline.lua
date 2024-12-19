5c988c53191cc7a76654b51f401ee910c493a92b
yuu-ux <yuugaehara@gmail.com>
2024-12-19 21:49:11 +0900

    :heavy_plus_sign: bufferline の設定を追加

---
+++ nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,27 @@
+return {
+    'akinsho/bufferline.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    keys = {
+        {
+            '<Tab>',
+            ':bnext<CR>',
+            mode = 'n',
+            silent = true,
+        },
+        {
+            '<S-Tab>',
+            ':bprevious<CR>',
+            mode = 'n',
+            silent = true,
+        },
+        {
+            '<F4>',
+            ':b #<CR>',
+            mode = 'n',
+            silent = true,
+        },
+    },
+    opts = {
+        termguicolors = true,
+    },
+}
