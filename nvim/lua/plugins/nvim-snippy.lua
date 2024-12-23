6b28697880769a40995a75c77b3724f1ffc92b4d
yuu-ux <yuugaehara@gmail.com>
2024-12-24 00:19:01 +0900

    :recycle: lsp 系のプラグインファイル分割

---
+++ nvim/lua/plugins/nvim-snippy.lua
@@ -0,0 +1,15 @@
+return {
+    'dcampos/nvim-snippy',
+    keys = {
+        {
+            '<Tab>',
+            'expand_or_advance',
+            silent = true,
+        },
+        {
+            '<S-Tab>',
+            'previous',
+            silent = true,
+        }
+    }
+}
