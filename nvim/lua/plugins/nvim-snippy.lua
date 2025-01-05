cf0cd3a91abd6f12d4642dbd8d4794dddc614c88
yuu-ux <yuugaehara@gmail.com>
2025-01-05 19:43:11 +0900

    :recycle: bufferline.lua とバッティングしていたため、削除

--- nvim/lua/plugins/nvim-snippy.lua
+++ nvim/lua/plugins/nvim-snippy.lua
@@ -1,15 +1,3 @@
 return {
     'dcampos/nvim-snippy',
-    keys = {
-        {
-            '<Tab>',
-            'expand_or_advance',
-            silent = true,
-        },
-        {
-            '<S-Tab>',
-            'previous',
-            silent = true,
-        }
-    }
 }
