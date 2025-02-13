205eccc7699905d89732d5f456bf537cc0584ed8
Yuga EHARA <yehara@moove.co.jp>
2025-02-13 17:24:31 +0900

    :heavy_plus_sign: mise の設定ファイルを追加

--- setting.sh
+++ setting.sh
@@ -2,9 +2,9 @@
 mkdir -p "${HOME}/.zsh"
 curl -o "${HOME}/.zsh/git-prompt.sh" https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 git clone --depth 1 https://github.com/unixorn/fzf-zsh-plugin.git
-git clone https://github.com/Aloxaf/fzf-tab
+git clone https://github.com/Aloxaf/fzf-tab ~/dotfiles/fzf-tab
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/dotfiles/.zsh/zsh-autosuggestions
-
+curl https://mise.run | sh
 # シンボリックリンク
 mkdir -p "${HOME}/.config"
 CURRENT_DIR=$(cd "$(dirname "$0")" && pwd)
