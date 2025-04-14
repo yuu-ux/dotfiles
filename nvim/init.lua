870606232aee5b69e26d061b17a63cd4f644c4ea
Yuga EHARA <yehara@moove.co.jp>
2025-04-14 11:57:07 +0900

    :bug: python が見つからないみたいなエラーがでていたため、修正

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,7 +5,7 @@ vim.scriptencoding = 'utf-8'
 require('config/options')
 require('config/keymaps')
 require('config/lazy')
-
+vim.g.python3_host_prog = '/Users/yehara/.local/share/mise/installs/python/3.12.2/bin/python3'
 vim.api.nvim_create_autocmd("TextYankPost", {
     group = vim.api.nvim_create_augroup("YankSync", { clear = true }),
     pattern = "*",
