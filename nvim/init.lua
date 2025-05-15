ae3520ee6a7d7ccad3a0bd6ac1f40150fbe1c111
yuu-ux <yuugaehara@gmail.com>
2025-05-15 21:22:14 +0900

    :bug: エラー出たから一旦コメントアウト

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,21 +6,11 @@ require('config/options')
 require('config/keymaps')
 require('config/lazy')
 
--- mason設定
 require('mason').setup()
-require('mason-lspconfig').setup {
-    ensure_installed = {'lua_ls', 'pyright', 'gopls'}
-}
+-- require('mason-lspconfig').setup {
+--     ensure_installed = {'lua_ls', 'pyright', 'gopls'}
+-- }
 
 require 'nvim-treesitter.configs'.setup {
-    -- A list of parser names, or 'all' (the listed parsers MUST always be installed)
     ensure_installed = {'c', 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline', 'go'}
 }
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-    group = vim.api.nvim_create_augroup("YankSync", { clear = true }),
-    pattern = "*",
-    callback = function()
-        vim.fn.setreg("+", vim.fn.getreg("@@"))
-    end,
-})
